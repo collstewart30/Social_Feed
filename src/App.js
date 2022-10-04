b1b6e09bceaed36271c91e1c2540cfdc442c4f76
Colleen Stewart <collstewart30@gmail.com>
2022-10-04 13:21:53 -0400

    Started table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, { useState } from 'react';
 
 
 function App() {
+
+  const [entries, setEntries] = useState([{name: 'Colleen Stewart', post: 'This is my first post!'}])
+
   return (
     <div className="App">
-
+      <h3 style={{margin: '1em'}}>Social
+      <small className= 'text-muted'>Feed</small></h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>colleen</td>
+            </tr>
+            <tr>
+              <td>first post</td>
+            </tr>
+          </tbody>
+        </table>
     </div>
   );
 }
